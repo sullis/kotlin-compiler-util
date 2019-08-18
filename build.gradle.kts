270caefd826fdbba466e6d71cf62745973cb9d3a
Sean Sullivan <github@seansullivan.com>
2019-08-18 01:37:50 -0400

    add [ build.gradle.kts ]

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  kotlin("jvm") version versions.kotlin apply false
+  id("org.jetbrains.dokka") version versions.dokka apply false
+  id("com.diffplug.gradle.spotless") version versions.spotless
+}
+
+spotless {
+  kotlin {
+    target("**/*.kt")
+    ktlint(versions.ktlint).userData(mapOf("indent_size" to "2"))
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
+}
+
+subprojects {
+  repositories {
+    mavenCentral()
+  }
+}
