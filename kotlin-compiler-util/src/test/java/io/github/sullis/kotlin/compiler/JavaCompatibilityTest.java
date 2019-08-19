7b5a7c0d47d57cbbfce100596bad695a00a5383f
Sean Sullivan <github@seansullivan.com>
2019-08-19 01:25:32 -0400

    add JavaCompatibilityTest.java

---
+++ kotlin-compiler-util/src/test/java/io/github/sullis/kotlin/compiler/JavaCompatibilityTest.java
@@ -0,0 +1,14 @@
+package io.github.sullis.kotlin.compiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JavaCompatibilityTest {
+    private KotlinCompiler compiler = KotlinCompiler.INSTANCE;
+
+    @Test
+    public void javaSanityCheck() {
+        KotlinCompiler.CompileResult result = compiler.compileSourceCode("data class Foo(val a: String)");
+        assertTrue(result.isSuccess());
+    }
+}
