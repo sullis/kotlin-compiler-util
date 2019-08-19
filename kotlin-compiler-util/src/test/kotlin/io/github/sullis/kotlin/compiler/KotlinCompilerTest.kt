f29f46fa7165aacf6191927b2980d9822de5b26e
Sean Sullivan <github@seansullivan.com>
2019-08-18 21:20:27 -0400

    improve KotlinCompilerTest.kt

--- kotlin-compiler-util/src/test/kotlin/io/github/sullis/kotlin/compiler/KotlinCompilerTest.kt
+++ kotlin-compiler-util/src/test/kotlin/io/github/sullis/kotlin/compiler/KotlinCompilerTest.kt
@@ -8,13 +8,20 @@ class KotlinCompilerTest {
     private val goodCode1 = "data class Foo(val a: String, val b: Int)"
     private val goodCode2 = "object Hello { fun echo(name: String): String { return name  } }"
 
+    private val badCode = arrayOf(
+            "import kotlin.collection.BogusList",
+            "{",
+            "fun echo()"
+    )
+
     @Test fun compileHappyPath() {
       val result = KotlinCompiler.compileSourceCode(goodCode1, goodCode2)
       assertTrue(result.isSuccess())
     }
 
-    @Test fun detectBadCode() {
-        val result = KotlinCompiler.compileSourceCode("import kotlin.collection.BogusList")
-        assertFalse(result.isSuccess())
+    @Test fun badCodeShouldNotCompile() {
+        for (code in badCode) {
+            assertFalse(KotlinCompiler.compileSourceCode(code).isSuccess())
+        }
     }
 }
