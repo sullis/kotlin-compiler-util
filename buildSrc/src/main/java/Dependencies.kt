34881687edb735913f1cdd8731db0b3aa515cda9
sullis <github@seansullivan.com>
2021-04-10 12:50:10 -0700

    maven publish 0.14.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@ object versions {
   const val kotlin = "1.4.10"
   const val spotless = "3.25.0"
   const val ktlint = "0.31.0"
-  const val mavenPublish = "0.8.0"
+  const val mavenPublish = "0.14.2"
   const val shadowPlugin = "5.0.0"
 }
 
